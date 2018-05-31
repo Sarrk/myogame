1ac821bb8af138d520cf6b93275f0a2822342d3c
csse4011 <Michael.Garthe@hotmail.com>
2018-05-31 23:25:24 +1000

    mpu integrated, not with udp tho

--- myogame/myogame.c
+++ myogame/myogame.c
@@ -2,7 +2,7 @@
  * This is the mainfile of the mygame project
  * 
  * Michael Redpath  43237166
- * Michael Garthe   XXXXXXXX
+ * Michael Garthe   43198856
  * Callum Sinclair  XXXXXXXX
  * Athon Milane     XXXXXXXX
  *
@@ -15,6 +15,11 @@
 #include "sys/etimer.h"
 #include "sys/rtimer.h"
 
+#include "lib/sensors.h"
+#include "mpu-9250-sensor.h"
+#include "ti-lib.h"
+#include "board-peripherals.h"
+
 #include <stdio.h>
 #include <string.h>
 
@@ -23,29 +28,69 @@
 
 #define DEBUG DEBUG_PRINT
 
-#define SAMPLE_FREQ 50
+#define SAMPLE_FREQ 1
 #define SAMPLE_INTERVAL CLOCK_SECOND / SAMPLE_FREQ
 
-PROCESS(adc_process, "ADC process");
+PROCESS(sensor_process, "Sensor process");
 PROCESS(udp_process, "UDP process");
-AUTOSTART_PROCESSES(&adc_process, &udp_process);
+AUTOSTART_PROCESSES(&sensor_process, &udp_process);         //sensor process will be started after calibration
 
 int cnt = 0;
 
 
+typedef struct myoData {
+    int adc;
+    int accX;
+    int accY;
+    int accZ;
+    int gyroX;
+    int gyroY;
+    int gyroZ;
+}  myoData;
+
+uint8_t mode = 0x07;        //will be changed in calibration
+
+myoData sensorData;
+
+
+
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(adc_process, ev, data) {
+PROCESS_THREAD(sensor_process, ev, data) {
     static struct etimer adc_et;
     PROCESS_BEGIN();
     
-    init_adc();
+    if (mode & 0x01) {
+        init_adc();
+    }
+    if (mode & 0x06) {
+        mpu_9250_sensor.configure(SENSORS_HW_INIT, 1);
+    }
 
     etimer_set(&adc_et, SAMPLE_INTERVAL);
     while(1) {
         PROCESS_YIELD();
         if(etimer_expired(&adc_et)) {
-            int adc_val = sample_adc();
-            process_post_synch(&udp_process, PROCESS_EVENT_CONTINUE, &adc_val);
+            if (mode & 0x01) {
+                sensorData.adc = sample_adc();
+            }
+            if (mode & 0x02) {
+                sensorData.accX = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_ACC_X);
+                sensorData.accY = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_ACC_Y);
+                sensorData.accZ = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_ACC_Z);
+            }
+            if (mode & 0x04) {
+                sensorData.gyroX = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_X);
+                sensorData.gyroY = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_Y);
+                sensorData.gyroZ = mpu_9250_sensor.value(MPU_9250_SENSOR_TYPE_GYRO_Z);
+            }
+            process_post_synch(&udp_process, PROCESS_EVENT_CONTINUE, &sensorData);
+            if ((mode  & 0x06) == 6) {
+                mpu_9250_sensor.configure(SENSORS_ACTIVE, MPU_9250_SENSOR_TYPE_ALL);
+            } else if (mode  & 0x02) {
+                mpu_9250_sensor.configure(SENSORS_ACTIVE, MPU_9250_SENSOR_TYPE_ACC_ALL);
+            } else if (mode & 0x04) {
+                mpu_9250_sensor.configure(SENSORS_ACTIVE, MPU_9250_SENSOR_TYPE_GYRO_ALL);
+            }
             etimer_restart(&adc_et);
         }
     }
@@ -61,23 +106,23 @@ PROCESS_THREAD(adc_process, ev, data) {
  *
  * @return None
  */
-PROCESS_THREAD(udp_process, ev, data) {
+ PROCESS_THREAD(udp_process, ev, data) {
     
     char msg[50];
     
     PROCESS_BEGIN();
     
     init_udp();
-    while(1) {
-        PROCESS_WAIT_EVENT();
+     while(1) {
+         PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_CONTINUE);
         if(data != NULL) {
-            sprintf(msg, "Received message %03d - %d", cnt++, *((int*) data));
+            sprintf(msg, "Received message %03d - %d", cnt++, sensorData.adc);
             udp_send(msg);
         }
-    }
+     }
 
-    PROCESS_END();
-}
+     PROCESS_END();
+ }
 
 
 
